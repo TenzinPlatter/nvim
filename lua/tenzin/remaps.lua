e5184cec1cc78d716f4aa333120903817d46a06a
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-04 21:24:58 +1000

    remove insert move with C+hjkl

--- lua/tenzin/remaps.lua
+++ lua/tenzin/remaps.lua
@@ -69,10 +69,10 @@ vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]])
 vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]])
 
 -- move in insert with CTRL + h/j/k/l
-vim.keymap.set('i', '<C-h>', '<left>')
-vim.keymap.set('i', '<C-j>', '<down>')
-vim.keymap.set('i', '<C-k>', '<up>')
-vim.keymap.set('i', '<C-l>', '<right>')
+-- vim.keymap.set('i', '<C-h>', '<left>')
+-- vim.keymap.set('i', '<C-j>', '<down>')
+-- vim.keymap.set('i', '<C-k>', '<up>')
+-- vim.keymap.set('i', '<C-l>', '<right>')
 
 -- move selection up/ down
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
