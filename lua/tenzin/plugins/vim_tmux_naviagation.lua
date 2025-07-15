204c51143ea8bd125a2b456a912db45cc49d8784
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-15 23:34:04 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/vim_tmux_naviagation.lua
+++ lua/tenzin/plugins/vim_tmux_naviagation.lua
@@ -13,9 +13,9 @@ return {
     "TmuxNavigatorProcessList",
   },
   keys = {
-    { "<M-h>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<M-j>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<M-k>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<M-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<M-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<M-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<M-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
   },
 }
