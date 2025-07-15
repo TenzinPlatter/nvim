eb39f35b903d84d9a9cebc5bc8dfc0649e370212
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-15 23:32:53 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/vim_tmux_naviagation.lua
+++ lua/tenzin/plugins/vim_tmux_naviagation.lua
@@ -1,5 +1,9 @@
 return {
   "christoomey/vim-tmux-navigator",
+  event = "VeryLazy",
+  init = function ()
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
   cmd = {
     "TmuxNavigateLeft",
     "TmuxNavigateDown",
