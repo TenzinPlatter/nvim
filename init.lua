af664ec0d1a6720300fc5857952bc360f5732000
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-23 22:12:01 +1000

    Auto-commit: modified 2 files

--- init.lua
+++ init.lua
@@ -16,6 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("tenzin.plugins", {})
 require("tenzin.remaps")
+require("tenzin.helpers")
 
 -- colorizer has to be setup after
 require('colorizer').setup()
--- lua/tenzin/helpers.lua
+++ lua/tenzin/helpers.lua
@@ -1,49 +1,92 @@
 local M = {}
 
 function M.show_hover_in_function_params()
-    local cursor_pos = vim.api.nvim_win_get_cursor(0)
-    local line = cursor_pos[1] - 1
-    local col = cursor_pos[2]
-    
-    local line_text = vim.api.nvim_buf_get_lines(0, line, line + 1, false)[1]
-    if not line_text then return end
-    
-    local function_start = nil
-    local paren_count = 0
-    local in_params = false
-    
-    for i = col, 1, -1 do
-        local char = line_text:sub(i, i)
-        if char == ')' then
-            paren_count = paren_count + 1
-        elseif char == '(' then
-            if paren_count == 0 then
-                function_start = i - 1
-                in_params = true
-                break
-            else
-                paren_count = paren_count - 1
-            end
-        end
-    end
-    
-    if in_params and function_start then
-        local function_name_end = function_start
-        while function_name_end > 0 do
-            local char = line_text:sub(function_name_end, function_name_end)
-            if char:match('[%w_]') then
-                function_name_end = function_name_end - 1
-            else
-                break
-            end
-        end
-        
-        if function_name_end < function_start then
-            vim.api.nvim_win_set_cursor(0, {line + 1, function_name_end})
-            vim.lsp.buf.hover()
-            vim.api.nvim_win_set_cursor(0, cursor_pos)
-        end
-    end
+	local cursor_pos = vim.api.nvim_win_get_cursor(0)
+	local line = cursor_pos[1] - 1
+	local col = cursor_pos[2]
+
+	local line_text = vim.api.nvim_buf_get_lines(0, line, line + 1, false)[1]
+	if not line_text then
+		return
+	end
+
+	local function_start = nil
+	local paren_count = 0
+	local in_params = false
+
+	for i = col, 1, -1 do
+		local char = line_text:sub(i, i)
+		if char == ")" then
+			paren_count = paren_count + 1
+		elseif char == "(" then
+			if paren_count == 0 then
+				function_start = i - 1
+				in_params = true
+				break
+			else
+				paren_count = paren_count - 1
+			end
+		end
+	end
+
+	if in_params and function_start then
+		local function_name_end = function_start
+		while function_name_end > 0 do
+			local char = line_text:sub(function_name_end, function_name_end)
+			if char:match("[%w_]") then
+				function_name_end = function_name_end - 1
+			else
+				break
+			end
+		end
+
+		if function_name_end < function_start then
+			vim.api.nvim_win_set_cursor(0, { line + 1, function_name_end })
+			vim.lsp.buf.hover()
+			vim.api.nvim_win_set_cursor(0, cursor_pos)
+		end
+	end
 end
+
+-- Create a highlight group for the temporary line highlight
+vim.api.nvim_set_hl(0, "TempLineHighlight", {
+	bg = "#3d4f5c", -- Adjust color to your preference
+	-- Or use: link = 'CursorLine' to match your existing cursor line color
+})
+
+-- Function to temporarily highlight current line
+local function highlight_current_line()
+	local line = vim.api.nvim_win_get_cursor(0)[1]
+	local ns_id = vim.api.nvim_create_namespace("temp_line_highlight")
+
+	-- Clear any existing highlights
+	vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+
+	-- Add highlight to current line (0-indexed, so line-1)
+	vim.defer_fn(function()
+		vim.api.nvim_buf_add_highlight(0, ns_id, "TempLineHighlight", line - 1, 0, -1)
+	end, 300)
+
+	-- Remove highlight after delay
+	vim.defer_fn(function()
+		vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+	end, 550) -- Adjust timing (milliseconds)
+end
+
+-- Autocmd for cursor movements that should trigger highlight
+vim.api.nvim_create_autocmd("CursorMoved", {
+	callback = function()
+		-- Check if this was a "big" jump by comparing with previous position
+		local current_line = vim.api.nvim_win_get_cursor(0)[1]
+		local last_line = vim.g.last_cursor_line or current_line
+
+		-- Trigger highlight if jump was more than 5 lines
+		if math.abs(current_line - last_line) > 5 then
+			highlight_current_line()
+		end
+
+		vim.g.last_cursor_line = current_line
+	end,
+})
 
 return M
