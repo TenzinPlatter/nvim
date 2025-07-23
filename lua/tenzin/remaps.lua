93bfaede8c4baafa1a7d5c43a6196effcbae5a78
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-23 14:53:46 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/remaps.lua
+++ lua/tenzin/remaps.lua
@@ -1,32 +1,32 @@
 -- lsp stuff
-vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename)
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
-vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references)
-vim.keymap.set('n', 'K', vim.lsp.buf.hover)
-vim.keymap.set('n', '<leader>sh', function()
-	local function is_clangd_attached(bufnr)
-		bufnr = bufnr or vim.api.nvim_get_current_buf()
-		for _, client in pairs(vim.lsp.get_active_clients({ bufnr = bufnr })) do
-			if client.name == "clangd" then
-				return true
-			end
-		end
-		return false
-	end
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>sh", function()
+  local function is_clangd_attached(bufnr)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = bufnr })) do
+      if client.name == "clangd" then
+        return true
+      end
+    end
+    return false
+  end
 
-	if is_clangd_attached() then
-		vim.cmd("LspClangdSwitchSourceHeader")
-	else
-		print('clangd not attached')
-	end
+  if is_clangd_attached() then
+    vim.cmd("ClangdSwitchSourceHeader")
+  else
+    print("clangd not attached")
+  end
 end)
 
 vim.keymap.set("n", "zi", "za")
 
 vim.keymap.set("n", "<C-S>", function()
-	vim.cmd("wall")
+  vim.cmd("wall")
 end)
 
 vim.keymap.set("n", "<C-l>", "xp")
@@ -46,18 +46,24 @@ vim.keymap.set("v", "<leader>p", '"+p')
 vim.keymap.set("n", "<leader>p", '"+p')
 
 -- Navigate quick fix list
-vim.keymap.set("n", "<leader>j", function() vim.cmd("cnext") end)
-vim.keymap.set("n", "<leader>k", function() vim.cmd("cprev") end)
+vim.keymap.set("n", "<leader>j", function()
+  vim.cmd("cnext")
+end)
+vim.keymap.set("n", "<leader>k", function()
+  vim.cmd("cprev")
+end)
 
 -- Navigate one screen line rather than actual line
 vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
 
 -- paste in insert from unnamed register
-vim.keymap.set('i', '<C-f>', '<C-r>"')
+vim.keymap.set("i", "<C-f>", '<C-r>"')
 
 -- clears highlight left after searching
-vim.keymap.set('n', '<C-c>', function() vim.cmd('nohlsearch') end)
+vim.keymap.set("n", "<C-c>", function()
+  vim.cmd("nohlsearch")
+end)
 
 -- make the window bigger vertically
 vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]])
@@ -81,6 +87,6 @@ vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("n", "ZZ", ":wqa<CR>")
 
 -- Show LSP hover for function when cursor is in parameter braces
-vim.keymap.set('i', '<A-i>', function()
-    require('tenzin.helpers').show_hover_in_function_params()
+vim.keymap.set("i", "<A-i>", function()
+  require("tenzin.helpers").show_hover_in_function_params()
 end)
