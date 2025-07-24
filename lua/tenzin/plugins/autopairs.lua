7dcf56273aee03e23ca3ce9f5a5a412f4de7e947
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-24 13:31:15 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/autopairs.lua
+++ lua/tenzin/plugins/autopairs.lua
@@ -1,5 +1,14 @@
 return {
-	'https://github.com/windwp/nvim-autopairs',
-	event = 'InsertEnter',
-	opts = {},
+	"https://github.com/windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = function()
+		require("nvim-autopairs").setup({
+			disable_filetype = { "TelescopePrompt", "vim" },
+		})
+
+		local npairs = require("nvim-autopairs")
+		local Rule = require("nvim-autopairs.rule")
+
+		npairs.add_rule(Rule("<", ">"))
+	end,
 }
