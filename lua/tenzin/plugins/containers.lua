d1a644b9d98a24a7c33f23322cf45d6ecfcf0153
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-24 17:04:22 +1000

    Auto-commit: modified 2 files

--- lua/tenzin/plugins/containers.lua
+++ lua/tenzin/plugins/containers.lua
@@ -1,3 +1,5 @@
 return {
-	"https://github.com/jamestthompson3/nvim-remote-containers",
+	"https://codeberg.org/esensar/nvim-dev-container",
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	opts = {},
 }
