f65b0c36183cfdb97012de19e7894ff2310e1578
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-29 13:47:19 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/lsp.lua
+++ lua/tenzin/plugins/lsp.lua
@@ -11,8 +11,16 @@ return {
 	},
 	{
 		"mason-org/mason.nvim",
-		opts = {}
-	},
+	opts = {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+        }
+			}
+    },
 	{
 		"mason-org/mason-lspconfig.nvim",
 		dependencies = {
