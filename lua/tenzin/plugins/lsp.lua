07189013e786b4eda3349155fa9d97b5556bb293
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-29 13:49:33 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/lsp.lua
+++ lua/tenzin/plugins/lsp.lua
@@ -1,32 +1,32 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		config = function ()
-			local lspconfig = require("lspconfig")
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
 
-			lspconfig.clangd.setup({
-				cmd = { "clangd", "--header-insertion=never" }
-			})
-		end
-	},
-	{
-		"mason-org/mason.nvim",
-	opts = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
-        }
-			}
+      lspconfig.clangd.setup({
+        cmd = { "clangd", "--header-insertion=never" },
+      })
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
     },
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"mason-org/mason.nvim"
-		},
-		opts = {}
-	}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mason-org/mason.nvim",
+    },
+    opts = {},
+  },
 }
