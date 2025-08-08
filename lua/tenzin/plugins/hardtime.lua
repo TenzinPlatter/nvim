d66f8a7a749a6197571109b624e7845012c000ad
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-08-08 18:41:37 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/hardtime.lua
+++ lua/tenzin/plugins/hardtime.lua
@@ -1,11 +1,13 @@
-return {
-	"m4xshen/hardtime.nvim",
-	lazy = false,
-	dependencies = { "MunifTanjim/nui.nvim" },
-	config = function()
-		require("hardtime").setup({})
-		vim.keymap.set("n", "<leader>hh", function()
-			vim.cmd("Hardtime toggle")
-		end, { desc = "Toggle Hardtime" })
-	end
-}
+-- return {
+-- 	"m4xshen/hardtime.nvim",
+-- 	lazy = false,
+-- 	dependencies = { "MunifTanjim/nui.nvim" },
+-- 	config = function()
+-- 		require("hardtime").setup({})
+-- 		vim.keymap.set("n", "<leader>hh", function()
+-- 			vim.cmd("Hardtime toggle")
+-- 		end, { desc = "Toggle Hardtime" })
+-- 	end
+-- }
+
+return {}
