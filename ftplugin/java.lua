a0ce8125f23b126d5a129134473aae913b417f41
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-08-27 12:59:22 +1000

    Auto-commit: modified 1 file

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,13 +1,12 @@
 local config = {
-    cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls') },
+  cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls') },
+    settings = {
+      java = {
+        signatureHelp = { enabled = true },
+      },
+    },
 
-		settings = {
-			java = {
-				signatureHelp = { enabled = true },
-			},
-		},
-
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+  root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
 
 require('jdtls').start_or_attach(config)
