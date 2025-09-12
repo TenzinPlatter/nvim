f0391b4928094e4c5cc8d493f20ea50ea28d81f0
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-12 15:44:47 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/lualine.lua
+++ lua/tenzin/plugins/lualine.lua
@@ -1,23 +1,12 @@
 return {
 	{
 		"f-person/git-blame.nvim",
-		-- load the plugin at startup
 		event = "VeryLazy",
-		-- Because of the keys part, you will be lazy loading this plugin.
-		-- The plugin will only load once one of the keys is used.
-		-- If you want to load the plugin at startup, add something like event = "VeryLazy",
-		-- or lazy = false. One of both options will work.
 		opts = {
-			-- your configuration comes here
-			-- for example
-			enabled = true, -- if you want to enable the plugin
-			message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
-			date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
-			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
-			options = {
-				globalstatus = true,
-			},
-			globalstatus = true,
+			enabled = true,
+			message_template = " <summary> • <date> • <author> • <<sha>>",
+			date_format = "%m-%d-%Y %H:%M:%S",
+			virtual_text_column = 1,
 		},
 	},
 	{
@@ -38,6 +27,9 @@ return {
 						{ prose.word_count, cond = prose.is_available },
 					},
 					lualine_y = { { "filename", path = 1 } },
+				},
+				options = {
+					globalstatus = true,
 				},
 			})
 		end,
