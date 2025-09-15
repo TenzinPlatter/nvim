bdf41f5cb5d29adef344797a28df89097fd5885a
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-15 17:01:18 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/diffview.lua
+++ lua/tenzin/plugins/diffview.lua
@@ -1,10 +1,10 @@
 return {
 	"https://github.com/sindrets/diffview.nvim",
 	config = function()
-		vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#34462F" })
-		vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#462F2F" })
-		vim.api.nvim_set_hl(0, "DiffChange", { bg = "#2F4146" })
-		vim.api.nvim_set_hl(0, "DiffText", { bg = "#463C2F" })
+		vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#4a1f1f" }) -- Dark red for deletions
+		vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#3a3a3a", fg = "#888888" }) -- Grey for your version
+		vim.api.nvim_set_hl(0, "DiffChange", { bg = "#3a3a1f" }) -- Keep change highlighting
+		vim.api.nvim_set_hl(0, "DiffText", { bg = "#4a4a1f", bold = true }) -- Keep text highlighting
 		vim.opt.fillchars:append("diff:╱")
 	end,
 }
