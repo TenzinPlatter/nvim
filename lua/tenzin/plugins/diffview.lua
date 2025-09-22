7271ad752367aae311fcd4c95efd1f024432924a
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-22 11:01:31 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/diffview.lua
+++ lua/tenzin/plugins/diffview.lua
@@ -1,6 +1,10 @@
 return {
 	"https://github.com/sindrets/diffview.nvim",
 	config = function()
+		require('diffview').setup({
+			enhanced_diff_hl = true
+		})
+
 		vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#28a745', fg = '#ffffff' })
 		vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#d73a49', fg = '#ffffff' })
 		vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#ffeaa7', fg = '#000000' })
