1e4d563a4bae32aaf512315067d54703cf829d65
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-22 10:52:08 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/diffview.lua
+++ lua/tenzin/plugins/diffview.lua
@@ -1,10 +1,14 @@
 return {
 	"https://github.com/sindrets/diffview.nvim",
 	config = function()
-		vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#2d1b29", fg = "#f7768e" }) -- Dark red bg, red text for deletions
-		vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#292e42", fg = "#565f89" }) -- Grey bg/text for your version
-		vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1a1b26" }) -- Tokyo Night background
-		vim.api.nvim_set_hl(0, "DiffText", { bg = "#394b70", bold = true }) -- Blue highlight for changed text
+		vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#28a745', fg = '#ffffff' })
+		vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#d73a49', fg = '#ffffff' })
+		vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#ffeaa7', fg = '#000000' })
+		vim.api.nvim_set_hl(0, 'DiffText', { bg = '#fdcb6e', fg = '#000000', bold = true })
+
+		-- Additional GitHub-style diff highlights
+		vim.api.nvim_set_hl(0, 'diffAdded', { fg = '#28a745' })
+		vim.api.nvim_set_hl(0, 'diffRemoved', { fg = '#d73a49' })
 		vim.opt.fillchars:append("diff:╱")
 	end,
 }
