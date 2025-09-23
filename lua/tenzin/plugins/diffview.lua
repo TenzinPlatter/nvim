bde57ec37cec870f4760037c2101f8271c31b6a5
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-23 14:38:06 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/diffview.lua
+++ lua/tenzin/plugins/diffview.lua
@@ -4,53 +4,9 @@ return {
 		require("diffview").setup({
 			enhanced_diff_hl = true,
 		})
-
-		-- Tokyo Night inspired diff colors for diffview
-		-- Soothing, low-contrast colors that are easy on the eyes
-
-		-- Standard diff highlights with Tokyo Night palette
-		vim.api.nvim_set_hl(0, "DiffAdd", {
-			bg = "#20303b",
-			fg = "#73daca",
-		})
-
-		vim.api.nvim_set_hl(0, "DiffDelete", {
-			bg = "#2d202a",
-			fg = "#f7768e",
-		})
-
-		vim.api.nvim_set_hl(0, "DiffChange", {
-			bg = "#32344a",
-			fg = "#7dcfff",
-		})
-
-		vim.api.nvim_set_hl(0, "DiffText", {
-			bg = "#394b70",
-			fg = "#c0caf5",
-			bold = true,
-		})
-
-		-- New diffview-specific highlight groups
-		vim.api.nvim_set_hl(0, "DiffviewDiffAddAsDelete", {
-			bg = "#2d202a",
-			fg = "#f7768e",
-		})
-
-		vim.api.nvim_set_hl(0, "DiffviewDiffDelete", {
-			bg = "#1f1f23",
-			fg = "#565f89",
-		})
-
-		-- Additional Tokyo Night diff styling
-		vim.api.nvim_set_hl(0, "diffAdded", { fg = "#9ece6a" })
-		vim.api.nvim_set_hl(0, "diffRemoved", { fg = "#f7768e" })
-		vim.api.nvim_set_hl(0, "diffChanged", { fg = "#7dcfff" })
-
-		-- Line numbers in diff view
-		vim.api.nvim_set_hl(0, "DiffviewStatusLine", {
-			bg = "#1a1b26",
-			fg = "#a9b1d6",
-		})
-		vim.opt.fillchars:append("diff:╱")
+		vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#34462F' })
+		vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#462F2F' })
+		vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#2F4146' })
+		vim.api.nvim_set_hl(0, 'DiffText', { bg = '#463C2F' })
 	end,
 }
