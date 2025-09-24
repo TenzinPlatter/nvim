4786444389c3aca3f916a1674f0bdae1371cb5c6
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-24 16:11:20 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/diffview.lua
+++ lua/tenzin/plugins/diffview.lua
@@ -7,10 +7,12 @@ return {
 		vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#34462F' })
 		vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#462F2F' })
 		vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#2F4146' })
-		vim.api.nvim_set_hl(0, 'DiffText', { bg = '#463C2F' })
+		vim.api.nvim_set_hl(0, 'DiffText', { bg = '#462F2F' })
+		-- to stop it from linking to DiffText
 		vim.cmd('highlight clear DiffTextAdd')
-		vim.api.nvim_set_hl(0, 'DiffTextAdd', { bg = '#462F2F' })
-		vim.api.nvim_set_hl(0, 'DiffviewDiffAddAsDelete', { bg = '#462F2F'})
-		vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', { bg = '#462F2F'})
+		-- vim.api.nvim_set_hl(0, 'DiffTextAdd', { bg = '#462F2F' })
+		vim.api.nvim_set_hl(0, 'DiffTextAdd', { bg = 'green' })
+		vim.api.nvim_set_hl(0, 'DiffviewDiffAddAsDelete', { bg = 'green'})
+		vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', { bg = 'green'})
 	end,
 }
