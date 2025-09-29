fdb3850524237910a72965c3c549016ec7e08ad7
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-29 10:58:25 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/persistance.lua
+++ lua/tenzin/plugins/persistance.lua
@@ -6,11 +6,11 @@ return {
   },
   keys = {
     -- load the session for the current directory
-    { "<leader>qs", function() require("persistence").load() end, desc = "Load session for current directory" },
+    { "<leader>ql", function() require("persistence").load() end, desc = "Load session for current directory" },
     -- select a session to load
     { "<leader>qS", function() require("persistence").select() end, desc = "Select a session to load" },
     -- load the last session
-    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Load the last session" },
+    { "<leader>qL", function() require("persistence").load({ last = true }) end, desc = "Load the last session" },
     -- stop Persistence => session won't be saved on exit
     { "<leader>qd", function() require("persistence").stop() end, desc = "Stop persistence" },
   },
