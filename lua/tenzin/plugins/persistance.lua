2997fe58ae3918eccb2f8d6f558b0d6cbe47166f
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-29 10:14:51 +1000

    Auto-commit: created 1 file, modified 1 file

---
+++ lua/tenzin/plugins/persistance.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre", -- this will only start session saving when an actual file was opened
+  opts = {
+    -- add any custom options here
+  },
+  keys = {
+    -- load the session for the current directory
+    { "<leader>qs", function() require("persistence").load() end, desc = "Load session for current directory" },
+    -- select a session to load
+    { "<leader>qS", function() require("persistence").select() end, desc = "Select a session to load" },
+    -- load the last session
+    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Load the last session" },
+    -- stop Persistence => session won't be saved on exit
+    { "<leader>qd", function() require("persistence").stop() end, desc = "Stop persistence" },
+  },
+}
