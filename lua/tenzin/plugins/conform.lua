19c1ef472c2f066ae7890b61a8445d86015c0bc6
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-29 13:18:44 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/conform.lua
+++ lua/tenzin/plugins/conform.lua
@@ -1,37 +1,50 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
-    formatters_by_ft = {
-      python = { "ruff_format" },
-      c = { "clang-format" },
-      cpp = { "clang-format" },
-      lua = { "stylua" },
-      yaml = { "yamlfmt" },
-      rust = { "rustfmt" },
-      ts = { "prettier" },
-      javascript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescript = { "prettier" },
-      typescriptreact = { "prettier" },
-      bash = { "beautysh" },
-      zsh = { "beautysh" },
-    },
-    formatters = {
-      ruff_format = {
-        args = { "format", "--stdin-filename", "$FILENAME", "-" },
-      }
-    },
-  },
-  keys = {
-    {
-      "<leader>=",
-      function()
-        require("conform").format()
-      end,
-      mode = { "n" },
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		formatters_by_ft = {
+			python = { "ruff_format" },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
+			lua = { "stylua" },
+			yaml = { "yamlfmt" },
+			rust = { "rustfmt" },
+			ts = { "prettier" },
+			javascript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+			bash = { "beautysh" },
+			zsh = { "beautysh" },
+		},
+		formatters = {
+			ruff_format = {
+				args = { "format", "--stdin-filename", "$FILENAME", "-" },
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>=",
+			function()
+				require("conform").format()
+			end,
+			mode = { "n" },
+		},
+		{
+			"<leader>=",
+			function()
+				require("conform").format({
+					range = {
+						start = vim.fn.line("'<"),
+						["end"] = vim.fn.line("'>"),
+					},
+				})
+			end,
+			mode = "v",
+			desc = "Format selection",
+		},
+	},
 }
