2125dae584f1dc45b798b3a0df20112d008e73fd
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-10-03 11:32:42 +1000

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/emmet.lua
+++ lua/tenzin/plugins/emmet.lua
@@ -1,7 +1,9 @@
 return {
   "olrtg/nvim-emmet",
   config = function()
-    vim.keymap.set({ "n", "v" }, '<leader>l', require('nvim-emmet').wrap_with_abbreviation)
+    vim.keymap.set({ "n", "v" }, "<leader>l", function()
+      require("nvim-emmet").wrap_with_abbreviation()
+      require("conform").format()
+    end)
   end,
 }
-
