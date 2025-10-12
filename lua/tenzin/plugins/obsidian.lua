cccdf8995e87964a288548a2edbef8aed7d6c697
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-10-13 09:36:57 +1100

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/obsidian.lua
+++ lua/tenzin/plugins/obsidian.lua
@@ -1,25 +1,25 @@
 return {
-  "epwalsh/obsidian.nvim",
+	"epwalsh/obsidian.nvim",
 	dependencies = {
 		"hrsh7th/nvim-cmp",
 		"nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
+		"nvim-lua/plenary.nvim",
 	},
-  version = "*",  -- recommended, use latest release instead of latest commit
-  lazy = true,
-  event = {
-    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    -- refer to `:h file-pattern` for more examples
-    "BufReadPre /home/tenzin/greenroom/notes*.md",
-    "BufNewFile /home/tenzin/greenroom/notes*.md",
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "greenroom",
-        path = "/home/tenzin/greenroom/notes",
-      },
-    },
-  },
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	event = {
+		-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+		-- refer to `:h file-pattern` for more examples
+		"BufReadPre /home/tenzin/gr/notes/*.md",
+		"BufNewFile /home/tenzin/gr/notes/*.md",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "greenroom",
+				path = "/home/tenzin/gr/notes",
+			},
+		},
+	},
 }
