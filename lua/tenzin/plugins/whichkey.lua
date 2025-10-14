90e499b08ea92fde22d63e726dcfab5e909a9b89
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-10-14 15:36:19 +1100

    Auto-commit: modified 2 files

--- lua/tenzin/plugins/whichkey.lua
+++ lua/tenzin/plugins/whichkey.lua
@@ -1,10 +1,15 @@
 return {
-  "folke/which-key.nvim",
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 250
-  end,
-  opts = {},
+	"folke/which-key.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 250
+	end,
+	opts = {
+		triggers_blacklist = {
+			n = { "<LeftMouse>", "<2-LeftMouse>", "<3-LeftMouse>", "<4-LeftMouse>", "<LeftDrag>", "<LeftRelease>" },
+			v = { "<LeftMouse>", "<2-LeftMouse>", "<3-LeftMouse>", "<4-LeftMouse>", "<LeftDrag>", "<LeftRelease>" },
+		},
+	},
 }
