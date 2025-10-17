6d615cb9a658e10bf10c3359eae7c601fd33e921
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-10-17 14:52:02 +1100

    Auto-commit: modified 2 files

--- lua/tenzin/plugins/fff.lua
+++ lua/tenzin/plugins/fff.lua
@@ -1,20 +1,22 @@
-return {
-  'dmtrKovalenko/fff.nvim',
-  build = function()
-    require("fff.download").download_or_build_binary()
-  end,
-  opts = {
-    debug = {
-      enabled = true,
-      show_scores = true,
-    },
-  },
-  lazy = false,
-  keys = {
-    {
-      "ff", -- try it if you didn't it is a banger keybinding for a picker
-      function() require('fff').find_files() end,
-      desc = 'FFFind files',
-    }
-  }
-}
+-- return {
+--   'dmtrKovalenko/fff.nvim',
+--   build = function()
+--     require("fff.download").download_or_build_binary()
+--   end,
+--   opts = {
+--     debug = {
+--       enabled = true,
+--       show_scores = true,
+--     },
+--   },
+--   lazy = false,
+--   keys = {
+--     {
+--       "ff", -- try it if you didn't it is a banger keybinding for a picker
+--       function() require('fff').find_files() end,
+--       desc = 'FFFind files',
+--     }
+--   }
+-- }
+
+return {}
