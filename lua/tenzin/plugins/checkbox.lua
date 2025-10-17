4fed49a7896379f41a2031959a4358300fcb1b01
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-10-17 13:59:03 +1100

    Auto-commit: modified 1 file

--- lua/tenzin/plugins/checkbox.lua
+++ lua/tenzin/plugins/checkbox.lua
@@ -1,10 +1,11 @@
 return {
 	"https://github.com/opdavies/toggle-checkbox.nvim",
-	opts = {},
 	keys = {
-		"<leader>tt",
-		function()
-			require("toggle-checkbox").toggle()
-		end,
+		{
+			"<leader>tt",
+			function()
+				require("toggle-checkbox").toggle()
+			end,
+		},
 	},
 }
