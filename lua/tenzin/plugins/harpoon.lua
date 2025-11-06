6e98af155df4595a545fd30209c1736047e4d4f4
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-11-06 11:01:51 +1100

    Modified 1
    Modified:
      lua/tenzin/plugins/harpoon.lua

--- lua/tenzin/plugins/harpoon.lua
+++ lua/tenzin/plugins/harpoon.lua
@@ -32,9 +32,6 @@ return {
     vim.keymap.set('n', '<leader>m', function() toggle_telescope(harpoon:list()) end, { desc = "Harpoon Menu" })
     vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = "Harpoon Add File" })
 
-    vim.keymap.set('n', "<C-N>", function() harpoon:list():next() end, { desc = "Harpoon Next File" })
-    vim.keymap.set('n', "<C-P>", function() harpoon:list():prev() end, { desc = "Harpoon Previous File" })
-
     vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end, { desc = "Harpoon Go to File 1" })
     vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end, { desc = "Harpoon Go to File 2" })
     vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end, { desc = "Harpoon Go to File 3" })
