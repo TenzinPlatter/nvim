138ac3fd7958858f2840e91e6f7fa72d35df0fe3
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-11-10 12:55:18 +1100

    Modified 1
    Modified:
      lua/tenzin/plugins/triforce.lua

--- lua/tenzin/plugins/triforce.lua
+++ lua/tenzin/plugins/triforce.lua
@@ -3,12 +3,9 @@ return {
   dependencies = {
     "nvzone/volt",
   },
-  config = function()
-    require("triforce").setup({
-      -- Optional: Add your configuration here
+	opts = {
       keymap = {
         show_profile = "<leader>tp", -- Open profile with <leader>tp
       },
-    })
-  end,
+	},
 }
